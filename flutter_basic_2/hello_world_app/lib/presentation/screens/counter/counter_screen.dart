dc6831b3f8da145283724d6e752a46e8bc4dc9d7
Diego <sdieg82@gmail.com>
2025-07-31 16:27:17 -0500

    counter working

--- flutter_basic_2/hello_world_app/lib/presentation/screens/counter/counter_screen.dart
+++ flutter_basic_2/hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
 
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(     
@@ -17,17 +24,22 @@ class CounterScreen extends StatelessWidget {
       body: Center(
           child: Column(
             mainAxisAlignment:MainAxisAlignment.center ,
-            children: [
-              Text('100',
+            children:[
+              Text('$clickCounter',
                     style: TextStyle(fontSize: 160,fontWeight: FontWeight.w100),
                     ),
-              Text('Cantidad de clicks',
-                    style: TextStyle(fontSize: 25),),
+                    ('$clickCounter' == '1')
+                      ? Text('Click', style: TextStyle(fontSize: 25))
+                      : Text('Clicks', style: TextStyle(fontSize: 25)),        
             ],
           ),
         ),   
     floatingActionButton: FloatingActionButton(
       onPressed: () {
+        setState(() {
+          clickCounter++;
+        });
+
         // Add your onPressed logic here
       },
       child: Icon(Icons.plus_one),
