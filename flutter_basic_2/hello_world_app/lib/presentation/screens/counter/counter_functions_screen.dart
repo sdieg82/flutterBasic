37d35373ec3a27c8292fe77e3b007ab46c86a785
Diego <sdieg82@gmail.com>
2025-08-01 10:13:13 -0500

    final counter

--- flutter_basic_2/hello_world_app/lib/presentation/screens/counter/counter_functions_screen.dart
+++ flutter_basic_2/hello_world_app/lib/presentation/screens/counter/counter_functions_screen.dart
@@ -7,71 +7,92 @@ class CounterFunctionsScreen extends StatefulWidget {
   State<CounterFunctionsScreen> createState() => _CounterFunctionsScreenState();
 }
 
-
 class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
-  int clickCounter=0;
+  int clickCounter = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(     
+    return Scaffold(
       appBar: AppBar(
-        title: const Text('Counter Functions',
-            style: TextStyle(
-              color: Colors.white)
-            ),
+        title: const Text(
+          'Counter Functions',
+          style: TextStyle(color: Colors.white),
+        ),
 
         centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 109, 118, 165),
+        backgroundColor: const Color.fromARGB(255, 52, 163, 80),
         actions: [
-          IconButton(onPressed: (){
+          IconButton(
+            onPressed: () {
+              setState(() {
+                clickCounter = 0;
+              });
+            },
+            icon: const Icon(Icons.refresh_rounded),
+          ),
+        ],
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$clickCounter',
+              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+            ('$clickCounter' == '1')
+                ? Text('Click', style: TextStyle(fontSize: 25))
+                : Text('Clicks', style: TextStyle(fontSize: 25)),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          CustomButton(icon: Icons.plus_one,
+          onPressed: (){
+            setState(() {
+              clickCounter++;
+            });
+          },),
+          SizedBox(height: 10),
+          CustomButton(icon: Icons.refresh_rounded,
+          onPressed: (){
             setState(() {
               clickCounter=0;
             });
-          }, icon: const Icon(Icons.refresh_rounded))
+          },),
+          SizedBox(height: 10),
+          CustomButton(icon:Icons.exposure_minus_1,
+          onPressed: (){
+            setState(() {
+              if(clickCounter==0)return;
+              clickCounter--;
+            });
+          },),
         ],
       ),
-      
-      body: Center(
-          child: Column(
-            mainAxisAlignment:MainAxisAlignment.center ,
-            children:[
-              Text('$clickCounter',
-                    style: TextStyle(fontSize: 160,fontWeight: FontWeight.w100),
-                    ),
-                    ('$clickCounter' == '1')
-                      ? Text('Click', style: TextStyle(fontSize: 25))
-                      : Text('Clicks', style: TextStyle(fontSize: 25)),        
-            ],
-          ),
-        ),   
-    floatingActionButton: Column(
-      
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        FloatingActionButton(
-          shape: CircleBorder(),
-          onPressed: () {
-        setState(() {
-          clickCounter++;
-        });
+    );
+  }
+}
 
-        // Add your onPressed logic here
-      },
-      child: Icon(Icons.plus_one),
-    ),
-    SizedBox(height: 10),
-    FloatingActionButton(
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback? onPressed;
+  const CustomButton({
+    super.key,
+    required this.icon, 
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
       shape: CircleBorder(),
-      onPressed: () {
-        setState(() {
-          clickCounter--;
-        });
-
-        // Add your onPressed logic here
-      },
-      child: Icon(Icons.exposure_minus_1),
-    )
-      ],
-    )
+      enableFeedback: true,
+      onPressed:onPressed,
+      elevation: 10,
+      child: Icon(icon),
     );
   }
 }
